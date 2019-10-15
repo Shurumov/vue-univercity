3f7305703dd788624d4ddebf8506f55b21961cde
Shurumov Aleksei <Shurumov@magora-systems.com>
2019-10-15 17:40:40 +0700

    [+]FRONTEND-269: custom carousel arrow buttons

--- src/components/CustomCarousel/index.vue
+++ src/components/CustomCarousel/index.vue
@@ -1,6 +1,7 @@
 <template>
   <slick
-          :options="settings">
+          :options="settings"
+  >
     <fragment v-if="typeItem === 'comments'">
       <ItemComment v-for="(item, index) of data"
                    :data="item"
@@ -18,10 +19,10 @@
 </template>
 
 <script>
+  import Slick from 'vue-slick';
+  import {Fragment} from 'vue-fragment';
   import ItemComment from './ItemComment';
   import ItemCourse from './ItemCourse';
-  import Slick from 'vue-slick';
-  import { Fragment } from 'vue-fragment';
 
   export default {
     name: 'CustomCarousel',
@@ -35,7 +36,7 @@
       ItemComment,
       ItemCourse,
       Slick,
-      Fragment
+      Fragment,
     },
     data() {
       return {
@@ -44,8 +45,8 @@
           infinite: true,
           slidesToScroll: 1,
           initialSlide: 0,
-          prevArrow: '<div class="carousel__arrow carousel__arrow-left"><</div>',
-          nextArrow: '<div class="carousel__arrow carousel__arrow-right">></div>',
+          prevArrow: '<div class="carousel__arrow carousel__arrow-left"><svg viewBox="64 64 896 896" width="20px" height="20px" fill="" version="1.1" xmlns="http://www.w3.org/2000/svg"> <path d="M724 218.3V141c0-6.7-7.7-10.4-12.9-6.3L260.3 486.8a31.86 31.86 0 0 0 0 50.3l450.8 352.1c5.3 4.1 12.9.4 12.9-6.3v-77.3c0-4.9-2.3-9.6-6.1-12.6l-360-281 360-281.1c3.8-3 6.1-7.7 6.1-12.6z"></path> </svg></div>',
+          nextArrow: '<div class="carousel__arrow carousel__arrow-right"><svg viewBox="64 64 896 896" width="20px" height="20px" fill="" version="1.1" xmlns="http://www.w3.org/2000/svg"> <path d="M765.7 486.8L314.9 134.7A7.97 7.97 0 0 0 302 141v77.3c0 4.9 2.3 9.6 6.1 12.6l360 281.1-360 281.1c-3.9 3-6.1 7.7-6.1 12.6V883c0 6.7 7.7 10.4 12.9 6.3l450.8-352.1a31.96 31.96 0 0 0 0-50.4z"></path> </svg></div>',
           ...this.options,
         }
       }
